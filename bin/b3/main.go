c01ff8f05dca1f518d96a28fa317d430a9c5c596
wangminxiang <ryougi.nevermore@hotmail.com>
2025-07-10 14:28:26 +0800

    mod cli

--- bin/b3/main.go
+++ bin/b3/main.go
@@ -1,7 +1,59 @@
+// Package main
+//
+// Usage:
+//
+// 1. brick: //go:generate go run -mod=mod github.com/brickingsoft/brick/bin/b3 -v .
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 func main() {
-	fmt.Println("hello")
+
+	ctx := context.Background()
+
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+
+	cmd := &cli.Command{
+		Name:                  "b3",
+		Usage:                 "brick cli application",
+		Description:           "brick by brick",
+		Version:               "v0.0.1",
+		EnableShellCompletion: true,
+		Commands: []*cli.Command{
+			{
+				Name:  "create",
+				Usage: "create a brick project",
+				Action: func(ctx context.Context, c *cli.Command) error {
+
+					return nil
+				},
+			},
+			{
+				Name:  "image",
+				Usage: "create Dockerfile",
+				Action: func(ctx context.Context, c *cli.Command) error {
+					return nil
+				},
+			},
+			{
+				Name:      "brick",
+				Usage:     "scan and generate code.",
+				UsageText: "use `//go:generate go run -mod=mod github.com/brickingsoft/brick/bin/b3 -v .` is well.",
+				Action: func(ctx context.Context, c *cli.Command) error {
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := cmd.Run(ctx, os.Args); err != nil {
+		log.Error("b3 failed !!!")
+	}
+
 }
