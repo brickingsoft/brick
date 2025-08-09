061a0db561e554bcf206f41529abe99d17e15994
wangminxiang <ryougi.nevermore@hotmail.com>
2025-08-09 20:16:55 +0800

    mod

--- logs/handler.go
+++ logs/handler.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/brickingsoft/brick/pkg/configs"
+	"github.com/brickingsoft/brick/pkg/mists"
 	"github.com/brickingsoft/brick/pkg/mosses"
 )
 
-type HandlerBuilder func(config *configs.Config) (handler mosses.Handler, err error)
+type HandlerBuilder func(config mists.Mist) (handler mosses.Handler, err error)
 
 var (
 	handlerBuilders = make(map[string]HandlerBuilder)
@@ -35,14 +35,11 @@ type StandardOutHandlerOptions struct {
 type HandlerOptions struct {
 	Name    string              `json:"name" yaml:"name"`
 	Async   AsyncHandlerOptions `json:"async" yaml:"async"`
-	Options configs.Raw         `json:"options" yaml:"options"`
+	Options mists.Raw           `json:"options" yaml:"options"`
 }
 
 func newHandler(options HandlerOptions) mosses.Handler {
-	config, configErr := configs.New(options.Options)
-	if configErr != nil {
-		panic(fmt.Sprintf("brick: unable to load log handler options options, %v", configErr))
-	}
+
 	name := strings.TrimSpace(options.Name)
 	if name == "" {
 		name = "stdout"
@@ -51,8 +48,12 @@ func newHandler(options HandlerOptions) mosses.Handler {
 	switch name {
 	case "stdout":
 		stdoutOptions := StandardOutHandlerOptions{}
-		if err := config.Decode(&options); err != nil {
-			panic(fmt.Sprintf("brick: unable to decode log handler options for %s, %v", name, err))
+		stdoutConfig, stdoutConfigErr := mists.New(options.Options)
+		if stdoutConfigErr != nil {
+			panic(fmt.Sprintf("brick: unable to read log handler options for %s, %v", name, stdoutConfigErr))
+		}
+		if decodeErr := stdoutConfig.Decode(&options); decodeErr != nil {
+			panic(fmt.Sprintf("brick: unable to decode log handler options for %s, %v", name, decodeErr))
 		}
 		encoder := strings.TrimSpace(stdoutOptions.Encoder)
 		if encoder == "" {
@@ -75,10 +76,9 @@ func newHandler(options HandlerOptions) mosses.Handler {
 		if builder == nil {
 			panic(fmt.Sprintf("brick: no log handler builder registered for %s", name))
 		}
-
-		handlerConfig, handlerConfigErr := configs.New(options.Options)
+		handlerConfig, handlerConfigErr := mists.New(options.Options)
 		if handlerConfigErr != nil {
-			panic(fmt.Sprintf("brick: unable to load log handler options options, %v", handlerConfigErr))
+			panic(fmt.Sprintf("brick: unable to load handler configuration for %s, %v", name, handlerConfigErr))
 		}
 		var buildErr error
 		handler, buildErr = builder(handlerConfig)
